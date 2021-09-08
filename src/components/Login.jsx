ca0e3eb4169d16a0876919d1aaea41ee2d88ff22
arseniypom <arpomazkov@bk.ru>
2021-09-08 11:39:05 +0300

    Added login functionality to Login component, including state handling, data validation, data sending and error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,84 @@
 import React from 'react'
 import {useRouteMatch, Link} from "react-router-dom";
+import classNames from 'classnames';
 
-import phoneIcon from '../images/icons/phone-icon.svg';
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
+
+import mailIcon from '../images/icons/mail-icon.svg';
 import googleImg from '../images/google.png';
 import vkImg from '../images/vk.png';
 
 function Login() {
   let match = useRouteMatch();
+  const auth = React.useContext(AuthContext)
+  const {isLoading, request, error, clearError} = useHttp()
+  const [errorMessage, setErrorMessage] = React.useState('')
+
+  const [form, setForm] = React.useState({
+    email: '',
+    password: ''
+  })
+  const [isEmailCorrect, setEmailCorrect] = React.useState(true)
+  const [isPasswordCorrect, setPasswordCorect] = React.useState(false)
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase())
+  }
+
+  const changeHandler = (e) => {
+    setForm({
+      ...form,
+      [e.target.id]:e.target.value
+    })
+  }
+
+  const blurHandler = (e) => {
+    const {id, value} = e.target
+    const label = document.querySelector(`[for=${id}]`)
+    if (form[id].length !== 0) {
+      label.classList.add('input-group__label--filled')
+    } else {
+      label.classList.remove('input-group__label--filled')
+    }
+
+    if (id === 'email') {
+      if (validateEmail(value)) {
+        setEmailCorrect(true)
+      } else {
+        setEmailCorrect(false)
+      }
+    }
+  }
+
+  const loginHandler = async () => {
+    // e.preventDefault()
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form})
+      auth.login(data.token, data.userId)
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
 
   return (
     <div className="register-login login">
       <h1 className="register-login__header">Вход</h1>
       <div className="register-login__form form">
         <div className="input-group">
-          <input className="input-group__input" type="text" id="phone" required />
-          <label for="phone" className="input-group__label">Номер телефона</label>
-          <img src={phoneIcon} alt="phone" className="input-group__icon" />
+          <input onChange={changeHandler} onBlur={blurHandler} className={classNames("input-group__input", {"input-group__input--error": !isEmailCorrect})} type="text" id="email" required />
+          <label htmlFor="email" className="input-group__label">Введите Ваш e-mail</label>
+          <img src={mailIcon} alt="email" className="input-group__icon" />
         </div>
+        <div className="input-group">
+            <input value={form.password} onChange={changeHandler} onBlur={blurHandler} className="input-group__input" type="password" id="password" required />
+            <label htmlFor="password" className="input-group__label">Введите пароль</label>
+          </div>
       </div>
-      <button className="register-login__button button button--primary">Войти</button>
+      <button onClick={loginHandler} className="register-login__button button button--primary" disabled={!isEmailCorrect || !form.password.length}>Войти</button>
+      <p className="form__error-message">{errorMessage}</p>
+      <p className="text-muted">Тестовая Учетная Запись: a1@mail.ru / 123456</p>
       <span className="register-login__line"></span>
       <h4 className="text-muted">Войти с помощью:</h4>
       <div className="register-login__alternative-signup">
