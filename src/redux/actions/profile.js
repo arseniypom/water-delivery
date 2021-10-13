12ccea4e9c983a4e260a518db7f52bb8bf5380d2
arseniypom <arpomazkov@bk.ru>
2021-10-13 18:53:47 +0300

    Added actions and reducers for managing user data requests

---
+++ src/redux/actions/profile.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export const setLoaded = (payload) => ({
+  type: 'SET_LOADED',
+  payload,
+})
+
+export const fetchProfileData = (id) => (dispatch) => {
+  dispatch(setLoaded(false))
+  axios.get(`/api/profile/data?id=${id}`).then(({data}) => {
+    dispatch(setLoaded(true))
+    dispatch(setProfileData(data))
+  })
+}
+
+export const setProfileData = (data) => ({
+  type: 'SET_PROFILE_DATA',
+  payload: data
+});
+
+export const resetProfileData = () => ({
+  type: 'RESET_PROFILE_DATA',
+});
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,12 +4,14 @@ import filtersReducer from './filters';
 import sortingReducer from './sorting';
 import productsReducer from './products';
 import cartReducer from './cart';
+import profileReducer from './profile';
 
 const rootReducer = combineReducers({
   filters: filtersReducer,
   sorting: sortingReducer,
   products: productsReducer,
   cart: cartReducer,
+  profile: profileReducer,
 });
 
 export default rootReducer;
