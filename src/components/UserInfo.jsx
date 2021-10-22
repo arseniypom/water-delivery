eded9577fcfb7633939b0e1e19606cd2401f97c7
arseniypom <arpomazkov@bk.ru>
2021-10-22 17:56:25 +0300

    Modified info-blocks

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -8,14 +8,18 @@ function UserInfo({userData, ordersList}) {
     <div className="user-profile__main">
       <h2 className="user-profile__main__header">Здравствуйте, {userData.firstName} {userData.lastName}!</h2>
       <div className="user-profile__main__info">
-        <h3>Ваш e-mail:</h3>
-        <p>{userData.email}</p>
-        <h3>Ваши заказы:</h3>
-        {
-          ordersList.length
-            ? <Orders ordersList={ordersList}/>
-            : <p>Перейдите <Link className="link--primary link--underlined" to="/">в магазин</Link>, чтобы сделать свой первый заказ</p>
-        }
+        <div className="user-profile__main__info__block">
+          <h3>Ваш e-mail:</h3>
+          <p>{userData.email}</p>
+        </div>
+        <div className="user-profile__main__info__block">
+          <h3>Ваши заказы:</h3>
+          {
+            ordersList.length
+              ? <Orders ordersList={ordersList}/>
+              : <p>Перейдите <Link className="link--primary link--underlined" to="/">в магазин</Link>, чтобы сделать свой первый заказ</p>
+          }
+        </div>
       </div>
     </div>
   )
